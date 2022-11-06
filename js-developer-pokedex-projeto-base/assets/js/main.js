121983dba7e84bf10d109650b3118b445548930d
jvmantovas <vmsjoao@gmail.com>
2022-11-05 23:15:42 -0300

    pokedex project js

--- js-developer-pokedex-projeto-base/assets/js/main.js
+++ js-developer-pokedex-projeto-base/assets/js/main.js
@@ -1,2 +1,47 @@
+const pokemonList = document.getElementById("pokemon-list");
 
-console.log('Sucesso!')
+const loadMoreButton = document.getElementById("loadMore");
+
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map(
+        (pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types
+                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .join("")}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+      )
+      .join("");
+    pokemonList.innerHTML += newHtml;
+  });
+}
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  const qttRecord = offset + limit;
+  if (qttRecord >= maxRecords) {
+    const newLimit = qttRecord - maxRecords;
+    loadPokemonItens(offset, newLimit);
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  }
+  loadPokemonItens(offset, limit);
+});
