cb02cdca9795421bd5f021b79e7ee5ff638aa836
jvmantovas <vmsjoao@gmail.com>
2022-11-11 00:33:47 -0300

    finished js project and started react lessons

---
+++ react/react-1/src/App.js
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <Button title="Entrar" />
+      <Button title="Sair" />
+      <Button title="Fechar" />
+      <Button title="Próximo" />
+    </div>
+  );
+}
+
+export default App;
