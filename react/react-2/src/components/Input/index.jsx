678802b0e30a615f7aa634786780df41fd697526
jvmantovas <vmsjoao@gmail.com>
2022-11-13 20:17:22 -0300

    react 2 style-components dio login done

---
+++ react/react-2/src/components/Input/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { InputContainer, IconContainer, InputText } from "./styles";
+
+const Input = ({ leftIcon, name, ...rest }) => {
+  return (
+    <InputContainer>
+      {leftIcon ? <IconContainer>{leftIcon}</IconContainer> : null}
+      <InputText {...rest} />
+    </InputContainer>
+  );
+};
+
+export { Input };
